ba6a81222194707ed4373b70cb60cb2d50d66f24
DavidCorzo <davidcorzo@ufm.edu>
2021-04-14 15:43:45 -0600

    terminado

---
+++ metodo_simplex/pruebas.py
@@ -0,0 +1,44 @@
+# f_o = {'z': 1, 'symbol': '=', 'X1': 2, 'X2': 3, 'X3': 0}
+# f_o = {'z': 1, 'symbol': '=', 'X1': 5, 'X2': 10, 'X3': 8}
+# f_o = {'z': 1, 'symbol': '=', 'X1': -2, 'X2': -1}
+# f_o = {'z': 1, 'symbol': '=', 'X1': 5, 'X2': 3}
+f_o = {'z': 1, 'symbol': '=', 'X1': 1, 'X2': 2, 'X3': 3, 'X4': -1}
+# 1
+# constraints = [
+#     {'X1': 2  , 'X2': -1  ,'X3': -1 , 'symbol': '>='    , 'c': 3 },
+#     {'X1': 1  , 'X2': -1  ,'X3':  1 , 'symbol': '>='    , 'c': 2 }
+# ]
+# urs=[0, 0]
+# m1:bool = False ## m primal
+# 2
+# constraints = [
+#     {'X1': 3  , 'X2': 5 , 'X3': 2 , 'symbol': '<='    , 'c': 60 },
+#     {'X1': 4  , 'X2': 4 , 'X3': 4 , 'symbol': '<='    , 'c': 72 },
+#     {'X1': 2  , 'X2': 4 , 'X3': 5 , 'symbol': '<='    , 'c': 100 }
+# ]
+# urs=[0, 0, 0]
+# m1:bool = True ## m primal
+# 3
+# constraints = [
+#     {'X1': -3  , 'X2': -1  , 'symbol': '<='    , 'c': -3 },
+#     {'X1': -4  , 'X2': -3  , 'symbol': '<='    , 'c': -6 },
+#     {'X1': -1 , 'X2': -2  , 'symbol': '<='    , 'c': -3 }
+# ]
+# urs=[0, 0]
+# m1:bool = True ## m primal
+# 4
+# constraints = [
+#     {'X1': 2  , 'X2': 4  , 'symbol': '<='    , 'c': 12 },
+#     {'X1': 2  , 'X2': 2  , 'symbol': '='    , 'c': 10 },
+#     {'X1': 5  , 'X2': 2  , 'symbol': '>='    , 'c': 10 }
+# ]
+# urs=[0, 0]
+# m1:bool = False ## m primal
+# # 5
+# constraints = [
+#     {'X1': 1  , 'X2': 2, 'X3': 3, 'X4': 0  , 'symbol': '='    , 'c': 15 },
+#     {'X1': 2  , 'X2': 1, 'X3': 5, 'X4': 0  , 'symbol': '='    , 'c': 20 },
+#     {'X1': 1  , 'X2': 2, 'X3': 1, 'X4': 1  , 'symbol': '='    , 'c': 10 }
+# ]
+# urs=[0, 0, 0, 0]
+# m1:bool = True ## m primal
